0a508f511aecd4126ebabda3fe2e429033507caa
lzp <1315363446@qq.com>
2024-01-14 21:49:18 +0700

    安全模块：设置访问速率（express-rate-limit）

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rateLimit = require('express-rate-limit');
+
 const { tourRouter } = require('./router/tourRouter');
 const userRouter = require('./router/userRouter');
 const { AppError } = require('./utils/errorHandle');
 const errorHandle = require('./controllers/errorController');
-
+// 安全：设置访问速率
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    limit: 100,
+    messgae: '访问受限，请稍后重试！！',
+  }),
+);
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/tour', tourRouter);

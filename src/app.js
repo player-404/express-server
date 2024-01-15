3e83cc21446f2d8d08668a7a1fc80fd1f0eaf084
lzp <1315363446@qq.com>
2024-01-15 15:57:26 +0700

    安全：防xss与sql注入完成

--- src/app.js
+++ src/app.js
@@ -6,13 +6,27 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const helmet = require('helmet');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const sanitizeHtml = require('sanitize-html');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoSanitize = require('express-mongo-sanitize');
 const { tourRouter } = require('./router/tourRouter');
 const userRouter = require('./router/userRouter');
 const { AppError } = require('./utils/errorHandle');
 const errorHandle = require('./controllers/errorController');
 
 app.use(express.json());
-app.use(morgan('dev'));
+//安全: 防止xss攻击
+app.use((req, res, next) => {
+  const clean = sanitizeHtml(JSON.stringify(req.body), {
+    allowedTags: [],
+    nonBooleanAttributes: [],
+  });
+  req.body = JSON.parse(clean);
+  next();
+});
+//安全: 防sql注入
+app.use(mongoSanitize());
 // 安全： 设置 http 响应头
 app.use(helmet());
 // 安全：设置访问速率
@@ -21,6 +35,7 @@ const limiter = rateLimit({
   limit: 100,
   messgae: '访问受限，请稍后重试！！',
 });
+app.use(morgan('dev'));
 app.use('/api', limiter);
 app.use('/api/tour', tourRouter);
 app.use('/api/user', userRouter);

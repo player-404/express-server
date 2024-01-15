b74f7ee8bd81ee4c3c239a4aabbc91d124047390
lzp <1315363446@qq.com>
2024-01-15 09:58:08 +0700

    安全：设置http头-helmet

--- src/app.js
+++ src/app.js
@@ -4,23 +4,26 @@ const app = express();
 const morgan = require('morgan');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const rateLimit = require('express-rate-limit');
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+const helmet = require('helmet');
 const { tourRouter } = require('./router/tourRouter');
 const userRouter = require('./router/userRouter');
 const { AppError } = require('./utils/errorHandle');
 const errorHandle = require('./controllers/errorController');
-// 安全：设置访问速率
-app.use(
-  rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 100,
-    messgae: '访问受限，请稍后重试！！',
-  }),
-);
+
 app.use(express.json());
 app.use(morgan('dev'));
-app.use('/tour', tourRouter);
-app.use('/user', userRouter);
+// 安全： 设置 http 响应头
+app.use(helmet());
+// 安全：设置访问速率
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 100,
+  messgae: '访问受限，请稍后重试！！',
+});
+
+app.use('/tour', limiter, tourRouter);
+app.use('/user', limiter, userRouter);
 
 //404处理
 app.use('*', (req, res, next) => {
